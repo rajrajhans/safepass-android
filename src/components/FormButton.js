72258b93e15a81d637bfccda45c5fb6bcea559e8
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-01 22:09:56 +0530

    Add FormButton and FormInput components

---
+++ src/components/FormButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {windowHeight, windowWidth} from '../utils/deviceDimensions';
+import {brandColor as brandColor} from '../../app.json';
+
+const FormButton = ({buttonTitle, ...rest}) => {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} {...rest}>
+      <Text style={styles.buttonText}>{buttonTitle}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default FormButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginTop: 10,
+    width: '100%',
+    height: windowHeight / 15,
+    backgroundColor: brandColor,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+});
