742c07a1039c0ec21c442fd428c65a2f39f5484b
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-01 21:48:52 +0530

    Update App.js to show Onboarding only on first launch

--- App.js
+++ App.js
@@ -1,25 +1,49 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
-import Home from './screens/Home';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {createStackNavigator} from '@react-navigation/stack';
+import Home from './screens/Home';
 import OnboardingScreen from './screens/Onboarding';
 
 const StackNav = createStackNavigator();
 
 const App = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
   useEffect(() => {
     SplashScreen.hide();
+
+    AsyncStorage.getItem('isFirstLaunchDone').then((val) => {
+      if (val == null) {
+        AsyncStorage.setItem('isFirstLaunchDone', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
   }, []);
 
-  return (
-    <NavigationContainer>
-      <StackNav.Navigator screenOptions={{headerShown: false}}>
-        <StackNav.Screen name={'Welcome'} component={OnboardingScreen} />
-        <StackNav.Screen name={'Home'} component={Home} />
-      </StackNav.Navigator>
-    </NavigationContainer>
-  );
+  if (isFirstLaunch == null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <StackNav.Navigator screenOptions={{headerShown: false}}>
+          <StackNav.Screen name={'Welcome'} component={OnboardingScreen} />
+          <StackNav.Screen name={'Home'} component={Home} />
+        </StackNav.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <StackNav.Navigator screenOptions={{headerShown: false}}>
+          <StackNav.Screen name={'Home'} component={Home} />
+        </StackNav.Navigator>
+      </NavigationContainer>
+    );
+  }
 };
 
 export default App;
