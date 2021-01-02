83a47eeff9fe0de8e84d211051d388ac0bf9539e
Raj Rajhans <raj.rajhans18@vit.edu>
2021-01-02 09:36:14 +0530

    Update LoadingProvider styles

--- src/components/LoadingProvider.js
+++ src/components/LoadingProvider.js
@@ -1,18 +1,38 @@
 import React, {createContext, useState} from 'react';
 import {Text} from 'react-native-elements';
-import {View} from 'react-native';
+import {StatusBar, View} from 'react-native';
+import styles from '../styles/Loading';
 
 export const LoadingContext = createContext();
 
 export const LoadingProvider = ({children}) => {
-  const [isLoading, setIsLoading] = useState('false');
+  const [isLoading, setIsLoading] = useState(true);
+  const [statusBarBg, setStatusBarBg] = useState('#3c0d99');
 
   return (
-    <LoadingContext.Provider value={{setIsLoading}}>
-      <View>
-        <Text>{isLoading}</Text>
-      </View>
-      {children}
+    <LoadingContext.Provider value={{setIsLoading, setStatusBarBg}}>
+      {isLoading ? (
+        <>
+          <StatusBar
+            backgroundColor={'rgba(0, 0, 0, 0.2)'}
+            barStyle={'light-content'}
+            animated={true}
+          />
+          {children}
+          <View style={styles.container}>
+            <Text>hello</Text>
+          </View>
+        </>
+      ) : (
+        <>
+          <StatusBar
+            backgroundColor={statusBarBg}
+            barStyle={'light-content'}
+            animated={true}
+          />
+          {children}
+        </>
+      )}
     </LoadingContext.Provider>
   );
 };
---
+++ src/styles/Loading.js
@@ -0,0 +1,19 @@
+import {brandColor as brandColor} from '../../app.json';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+});
+
+export default styles;
